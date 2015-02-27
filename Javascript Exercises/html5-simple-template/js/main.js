ccdef92caa82bc946a04f99c63ebec68e6132012
CafeConVega <joshua.vega.r@gmail.com>
2015-02-26 22:55:32 -0500

    finished up to n9

--- Javascript Exercises/html5-simple-template/js/main.js
+++ Javascript Exercises/html5-simple-template/js/main.js
@@ -1,4 +1,4 @@
-console.log("n1 = " + max(200, 4) + " n2 = " + maxOfThree(10, 2990, 30) + " n3 = " + vowelCheck("y") + " n4 = " + translate("joshua") + " n5 multiply = " + multiply(1,2,3,4) + " n5 sum = " + sum(1,2,3,4,5));
+console.log("n1 = " + max(200, 4) + " n2 = " + maxOfThree(10, 2990, 30) + " n3 = " + vowelCheck("y") + " n4 = " + translate("joshua") + " n5 multiply = " + multiply(1, 2, 3, 4) + " n5 sum = " + sum(1, 2, 3, 4) + " n6 = " + reverse("joshua") + " n7 = " + randMax100() + " n8 = " + findLongestWord("cat", "cats", "catss", "catsssss", "catsssssssssssssssss") + " n9 = " + findLongWords(5, "cat", "cats", "catss", "catsssss", "catsssssssssssssssss", "tttttttttt"));
 
 
 //n1
@@ -51,7 +51,7 @@ function translate(abc) {
 function multiply() {
     var multiply;
     var total = 1;
-    for (i = 1; i <= arguments.length-1; i = i+2) {
+    for (i = 1; i <= arguments.length - 1; i = i + 2) {
         multiply = arguments[i - 1] * arguments[i];
         total = total * multiply;
     }
@@ -64,4 +64,43 @@ function sum() {
         sum += arguments[i];
     }
     return sum;
+}
+
+//n6
+function reverse(abc) {
+    var strng = abc;
+    var array = strng.split('');
+    array = array.reverse();
+    return array.join("");
+}
+
+//n7
+function randMax100() {
+    return Math.max(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
+}
+
+//n8
+function findLongestWord() {
+    var strng = [];
+    var length = [];
+    for (i = 0; i < arguments.length; i++) {
+        strng[i] = arguments[i];
+        length[i] = strng[i].length;
+    }
+    return Math.max.apply(Math, length);
+}
+
+//n9
+function findLongWords(i) {
+    var array = [];
+    for (j = 1; j < arguments.length; j++) {
+            array[j - 1] = arguments[j];
+    }
+     for (jj = 0; jj < array.length; jj++) {
+      if (array[jj].length <= arguments[0]) {
+            array.splice(jj, 1);
+          jj--;
+        }
+     } 
+    return array;
 }
